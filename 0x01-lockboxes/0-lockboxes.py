5b0b7b71648b7821cb3b3a4a20fce7e760b1e937
Benonking <benonkighoma@gmail.com>
2023-08-06 17:54:09 +0300

    check if all boxes can be unlocked
    interview prep qtn

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+Module 0-lockboxes
+Function to determine if all lock box can be opened
+'''
+
+
+def canUnlockAll(boxes):
+    '''
+    check if index of list is contained in list of lists
+    '''
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+    for key in box:
+        if key < len(boxes) and key not in unlocked and key != box_id:
+            unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
