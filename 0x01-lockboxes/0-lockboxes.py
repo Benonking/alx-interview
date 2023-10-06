4d2374daf19a3f1995b44b3f84de01d8697f1a73
Benonking <benonkighoma@gmail.com>
2023-10-06 09:36:13 +0300

    solution to lock box problem BFS approach

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,9 +3,10 @@
 Module 0-lockboxes
 Function to determine if all lock box can be opened
 '''
+from typing import List
 
 
-def canUnlockAll(boxes):
+def canUnlockAll(boxes: List[List]) -> bool:
     '''
     check if index of list is contained in list of lists
     '''
@@ -19,3 +20,22 @@ def canUnlockAll(boxes):
     if len(unlocked) == len(boxes):
         return True
     return False
+
+# BFS approach
+# from collections import deque
+# from typing import List
+
+# def canUnlockAll(boxes: List[List[int]]) -> bool:
+#     n = len(boxes)
+#     visited = [False] * n
+#     visited[0] = True
+
+#     queue = deque([0])
+
+#     while queue:
+#         current_box = queue.popleft()
+#         for key in boxes[current_box]:
+#             if not visited[key]:
+#                 visited[key] = True
+#                 queue.append(key)
+#    return all(visited)
