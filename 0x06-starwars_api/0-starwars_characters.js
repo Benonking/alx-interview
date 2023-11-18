00c01f84256eda73be8383de7f2ba0670a38ee52
Benonking <benonkighoma@gmail.com>
2023-11-18 09:15:18 +0300

    return response in order of index

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,23 +5,23 @@ const request = require('request');
 
 const id = process.argv[2];
 
-function getMovieCharacters (id) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-  request(url, (err, resp, body) => {
-    if (err) {
-      return;
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+request(url, (err, resp, body) => {
+  if (err) {
+    return;
+  }
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
+  printCha(characters, 0);
+});
+
+function printCha (ch, indx) {
+  request(ch[indx], (err, res, body) => {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (indx + 1 < ch.length) {
+        printCha(ch, indx + 1);
+      }
     }
-    const filmData = JSON.parse(body);
-    const characters = filmData.characters;
-    characters.forEach(chaURL => {
-      request(chaURL, (err, resp, body) => {
-        if (err) {
-          return;
-        }
-        const chdata = JSON.parse(body);
-        console.log(chdata.name);
-      });
-    });
   });
 }
-getMovieCharacters(id);
