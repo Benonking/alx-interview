89e77aae1d1832d1e9c7653a7b4a1f2269eea926
Benonking <benonkighoma@gmail.com>
2023-11-30 12:57:08 +0300

    calculate island perimeter recursively using DFS

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''
+Module Island perimeter
+'''
+
+
+def island_perimeter(grid):
+    '''
+    Args:
+        grid: list of intergers
+            0- represents water
+            1 rep land
+        Approach: Depth first search
+    Returns: perimeter of an island described in a grid
+    '''
+    # get matrix dimensions
+    if not grid or not grid[0]:
+        return 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    def dfs(row, col):
+        if row < 0 or row >= rows or col < 0 or col > cols or grid[row][col] == 0:
+            return 1
+        if grid[row][col] == -1:
+            return 0  # already visited
+        
+        # set node to already visited
+        grid[row][col] = -1
+        perimeter = dfs(row-1, col) + dfs(row+1, col) + \
+            dfs(row, col - 1) + dfs(row, col + 1)
+        return perimeter
+
+    # Find the first land cell to start the traversal
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                return dfs(i, j)
+    # No island found
+    return 0
